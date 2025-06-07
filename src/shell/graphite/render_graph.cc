8dc3563245d57736486b215ca35f77d9540e7f99
MΛX <mxcop.dev@gmail.com>
2025-06-07 16:45:09 +0200

    feat: convert to library

--- src/shell/graphite/render_graph.cc
+++ src/shell/graphite/render_graph.cc
@@ -5,6 +5,6 @@
 ComputeNode& RenderGraph::add_compute_pass(std::string_view label, std::string_view file_alias) {
     /* Create the new compute node, and insert it into the nodes list. */
     ComputeNode* new_node = new ComputeNode(label, file_alias);
-    nodes.emplace_back(new_node);
+    nodes.emplace_back((Node*)new_node);
     return *new_node;
 }
