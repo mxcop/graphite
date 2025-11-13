ae7612d7e25d5572f34ece743523eecd4cb7d028
Gikster007 <giksterbolba@gmail.com>
2025-11-13 10:00:10 +0100

    fix: removed useless compile command argument

--- samples/testing/kernels/compile.cmd
+++ samples/testing/kernels/compile.cmd
@@ -1,7 +1,7 @@
-slangc test.slang -profile sm_6_0 -target spirv -o test.spv -entry entry -bindless-space-index 1
-slangc image.slang -profile sm_6_0 -target spirv -o image.spv -entry entry -bindless-space-index 1
-slangc buffer-fill.slang -profile sm_6_0 -target spirv -o buffer-fill.spv -entry entry -bindless-space-index 1
+slangc test.slang -profile sm_6_0 -target spirv -o test.spv -entry entry
+slangc image.slang -profile sm_6_0 -target spirv -o image.spv -entry entry
+slangc buffer-fill.slang -profile sm_6_0 -target spirv -o buffer-fill.spv -entry entry
 
-slangc graphics-test.slang -profile vs_6_0 -target spirv -o graphics-test-vert.spv -entry vertex_entry -bindless-space-index 1
-slangc graphics-test.slang -profile ps_6_0 -target spirv -o graphics-test-frag.spv -entry fragment_entry -bindless-space-index 1
+slangc graphics-test.slang -profile vs_6_0 -target spirv -o graphics-test-vert.spv -entry vertex_entry
+slangc graphics-test.slang -profile ps_6_0 -target spirv -o graphics-test-frag.spv -entry fragment_entry
 pause
