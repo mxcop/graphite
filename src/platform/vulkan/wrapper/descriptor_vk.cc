4d9a72849918eb823c2ae1b396c94204c49b0c7f
George Bolba <giksterbolba@gmail.com>
2025-11-25 15:31:17 +0100

    fix: return value

--- src/platform/vulkan/wrapper/descriptor_vk.cc
+++ src/platform/vulkan/wrapper/descriptor_vk.cc
@@ -113,7 +113,7 @@ VkDescriptorSetLayoutBinding sampler_layout(u32 slot, const Dependency& dep) {
 Result<void> node_push_descriptors(const RenderGraph& rg, const Pipeline& pipeline, const Node &node) {
     /* Allocate memory for all the write commands and descriptors */
     const u32 binding_count = (u32)node.dependencies.size();
-    if (binding_count < 1) return;
+    if (binding_count < 1) return Ok();
 
     std::vector<VkWriteDescriptorSet> writes(binding_count);
     std::vector<VkDescriptorBufferInfo> buffer_info(binding_count);
